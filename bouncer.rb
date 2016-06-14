a69e36e52171a15be4cb9b650bbe0640eb14eafe
Robert Luckfield <rluck0419@gmail.com>
2016-06-14 13:43:37 -0500

    Added functionality for Adventurer mode - the program now loops until the user gives the command to quit

--- bouncer.rb
+++ bouncer.rb
@@ -9,16 +9,34 @@
 #   - if the user is over 21, the user can drink
 #   - if the user is over 25, the user can rent a car
 
-puts "How old are you?"
-print "<Please enter your age:> "
-user_age = gets.chomp.to_i
+loop do
+  puts "How old are you?"
+  print "<Please enter your age, or 'q' to quit:> "
+  user_age = gets.chomp
 
-if user_age < 18
-  puts "Sorry! You're too young to do anything."
-elsif user_age < 21
-  puts "Congradulations! You're old enough to vote and smoke!"
-elsif user_age < 25
-  puts "Congradulations! You're old enough to vote, smoke, and drink!"
-else
-  puts "Congradulations! You're old enough to vote, smoke, drink, and rent a car."
+  # check if user wants to quit
+  if user_age == 'q'
+    break
+  end
+
+  # convert the user's age to an integer (having confirmed the user does not wish to quit)
+  user_age = user_age.to_i
+
+# failsafe for 0-years-old and any other non-interger inputs
+  if user_age.zero?
+    puts "Sorry? I didn't understand that."
+    print "<Please enter your age, or 'q' to quit:> "
+    user_age = gets.chomp.to_i
+# check each age group - 4 possibilities (under 18, under 21, under 25, over 25)
+  elsif user_age < 18
+    puts "Sorry! You're too young to do anything."
+  elsif user_age < 21
+    puts "Congradulations! You're old enough to vote and smoke!"
+  elsif user_age < 25
+    puts "Congradulations! You're old enough to vote, smoke, and drink!"
+  else
+    puts "Congradulations! You're old enough to vote, smoke, drink, and rent a car."
+  end
+  # creates spacing before restarting the loop
+  puts
 end
