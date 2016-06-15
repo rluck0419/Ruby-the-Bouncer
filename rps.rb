d97d5d866ce7f818a2d3ba8fee0b13871abf39d6
Robert Luckfield <rluck0419@gmail.com>
2016-06-14 21:08:42 -0500

    adventurer mode. removed uncessary commits, added a few clarifying comments, and provided a more detailed output for the user.

--- rps.rb
+++ rps.rb
@@ -1,61 +1,52 @@
-# ask player 1 for their choice of "rock", "paper", or "scissors"
-# randomly select the computer's choice
-# Output of results: "Player: ROCK vs Computer: SCISSORS ----- PLAYER WINS"
-#
-# outcomes
-#   rock beats scissors
-#   paper beats rock
-#   scissors beats paper
-
+# possible outcomes and counters for player 1's W-L-T
 choices = ["ROCK", "PAPER", "SCISSORS"]
 wins = 0
 losses = 0
 ties = 0
 
+# play the game 1000 times & record wins-losses-ties
 1000.times do
   computer_choice1 = choices.sample
   computer_choice2 = choices.sample
 
   if computer_choice1 == "ROCK"
-    # tie
     if computer_choice2 == "ROCK"
       ties += 1
-    #lose
     elsif computer_choice2 == "PAPER"
       losses += 1
-    #win
-    else
+    else # player 2 chooses "SCISSORS"
       wins += 1
     end
   elsif computer_choice1 == "PAPER"
-    #tie
     if computer_choice2 == "PAPER"
       ties += 1
-    #lose
     elsif computer_choice2 == "SCISSORS"
       losses += 1
-    #win
-    else
+    else # player 2 chooses "ROCK"
       wins += 1
     end
-  else #player choice is "SCISSORS"
-    #tie
+  else # player 1 chooses "SCISSORS"
     if computer_choice2 == "SCISSORS"
       ties += 1
-    #lose
     elsif computer_choice2 == "ROCK"
       losses += 1
-    #win
-    else
+    else # player 2 chooses "PAPER"
       wins += 1
     end
   end
 end
 
+# calculate player 2's W-L-T
 wins2 = 1000 - wins - ties
 losses2 = 1000 - losses - ties
 ties2 = 1000 - wins - losses
 
+# show user the results
 puts "1000 matches played! W-L-T"
+puts
 puts "Computer 1: #{wins}-#{losses}-#{ties}"
 puts "Computer 2: #{wins2}-#{losses2}-#{ties2}"
+puts
+puts "Computer 1 wins!" if wins > wins2
+puts "Computer 2 wins!" if wins < wins2
+puts "Wow! 1000 games later and it's a tie!" if wins == wins2
