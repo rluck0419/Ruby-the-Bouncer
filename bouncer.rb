bfcb42d1155d183d3583069b27b42dd0e5cd1838
Robert Luckfield <rluck0419@gmail.com>
2016-06-14 20:53:36 -0500

    replaced adventurer version with epic mode version

--- bouncer.rb
+++ bouncer.rb
@@ -1,37 +1,70 @@
-# bouncer.rb
-#
-# - write a method "check_age" that runs every time the script is run
-# - ask for the user's age
-# - tell the user what it can do based on age:
-#   - if the user is under 18, the user can do nothing
-#   - if the user is over 18, the user can vote
-#   - if the user is over 18, the user can smoke
-#   - if the user is over 21, the user can drink
-#   - if the user is over 25, the user can rent a car
-
-loop do
-  puts "How old are you?"
-  print "<Please enter your age, or 'q' to quit:> "
-  user_age = gets.chomp
-
-  # check if user wants to quit
-  if user_age == 'q'
-    break
-  end
-
-  # convert the user's age to an integer (having confirmed the user does not wish to quit)
-  user_age = user_age.to_i
-
- # Optional failsafe for 0-years-old and any other non-interger inputs
-  if user_age < 18
-    puts "Sorry! You're too young to do adult things."
-  elsif user_age < 21
+def us_age_check(age)
+  if age < 18
+    puts "Sorry! You're too young to do adult things!"
+  elsif age < 21
     puts "Congratulations! You're old enough to vote and smoke!"
-  elsif user_age < 25
+  elsif age < 25
     puts "Congratulations! You're old enough to vote, smoke, and drink!"
   else
-    puts "Congratulations! You're old enough to vote, smoke, drink, and rent a car."
+    puts "Congratulations! You're old enough to vote, smoke, drink, and rent a car!"
   end
-  # creates spacing before restarting the loop
-  puts
+end
+
+def uk_age_check(age)
+  if age < 16
+    puts "Sorry! You're too young to do adult things!"
+  elsif age < 18
+    puts "Are you with your parents?"
+    print "<Please enter (Y)es or (N)o:> "
+    with_parents = gets.chomp.upcase
+
+    if with_parents == 'Y'
+      puts "Congratulations! You're old enough to drink!"
+    elsif with_parents == 'N'
+      puts "Sorry! You're too young to do adult things!"
+    else unknown_input
+    end
+
+  elsif age < 25
+    puts "Congratulations! You're old enough to vote, smoke, and drink!"
+  else
+    puts "Congratulations! You're old enough to vote, smoke, drink and rent a car!"
+  end
+end
+
+def unknown_input
+  puts "Sorry? I didn't catch that."
+end
+
+loop do
+  puts "Are you in the US or the UK?"
+  print "<Please enter (US) or (UK), or (Q) to quit:> "
+  user_country = gets.chomp.upcase
+  if user_country == 'Q'
+    break
+  elsif user_country != 'US' && user_country != 'UK'
+    unknown_input
+  else
+
+    puts "How old are you?"
+    print "<Please enter your age, or (Q) to quit:> "
+    user_age = gets.chomp
+
+    if user_age == 'q'
+      break
+    end
+
+    user_age = user_age.to_i
+    if user_age == 0
+      unknown_input
+    else
+      case user_country
+      when 'US' then us_age_check(user_age)
+      when 'UK' then uk_age_check(user_age)
+      else
+        unknown_input
+      end
+      puts
+    end
+  end
 end
